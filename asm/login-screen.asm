c4f9de78f7013e53554419721ef252ba5a66d1d6
savolla <savolla@protonmail.com>
2020-04-12 20:17:27 +0300

    improved documentation and added new functions

--- asm/login-screen.asm
+++ asm/login-screen.asm
@@ -15,58 +15,66 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-
-
-
-;# RUNTIME #
 [org 0x7c00]						;; magic number. see: D.01
 
-mov bx, str_welcome					;; load bx with address of str_welcome variable
+mov bx, banner						;; load bx with address of str_welcome variable
 call print_nl						;; print str_welcome to the tty
+
 
 mov bx, str_username				;; load bx with address of str_username variable
 call print							;; print str_welcome to the tty
 call get_one_line_input				;; let user to input his/her username
 
+
 mov bx, str_password				;; load bx with address of str_password variable
-call print							;; print str_welcome to the tty
+call print							;; print banner to the tty
 call get_one_line_input				;; let user to input his/her password
+
+mov cl, 0x02						;; from which sector we want to start reading
+mov al, 0x01                        ;; how many sectors to read
+call load_into_memory
 
 jmp $								;; think like system("pause"). see: D.02
 
+%include "utils.asm"				;; includes useful utility functions from utils.asm
 
+;; real banner.. but boot sector can't handle that (yet :))
+;; banner: 
+;; 		db " ▄▄▄▄▄▄   ▄███▄      ▄   ▄█    ▄▄▄▄▀ ▄  █ ", 0x0a, 0x0d, "▀   ▄▄▀   █▀   ▀      █  ██ ▀▀▀ █   █   █ ", 0x0a, 0x0d, " ▄▀▀   ▄▀ ██▄▄    ██   █ ██     █   ██▀▀█ ", 0x0a, 0x0d, " ▀▀▀▀▀▀   █▄   ▄▀ █ █  █ ▐█    █    █   █ ", 0x0a, 0x0d, "          ▀███▀   █  █ █  ▐   ▀        █  ", 0x0a, 0x0d, "                  █   ██              ▀   ", 0x00
+ 
 
-
-
-;# INCLUSION #
-%include "utils.asm"				;; file that includes useful utility functions
-
-
-
-
-
-;# VARIABLES #
-str_welcome: db "zenith glances at you with its foggy eyes", 0x0
+banner: db "zenith glances at you with its foggy eyes", 0x0
 str_username: db "name: ", 0x0
 str_password: db "key : ", 0x0
-
-times 510 - ( $ - $$ ) db 0
-dw 0xaa55							;; end of boot sector. see D.03
-
+second_sector_message:
+	db "hello from second sector", 0x0
 
 
+times 510 - ( $ - $$ ) db 0x0		;; padding with zeros to make file 512 bytes
+dw 0xaa55							;; end of boot sector. (see D.03)
 
 
-;# DOCUMENTATION #
-; D.01: 0x7c00 is a special address in x86 architecture. it represents a boot sector.
-;		where our code will be placed into memory after you press power button of your
-;		 PC
-;		
-; D.02: this is special for nasm (netwide assembler). '$' symbol represents the "current
-; 		address". it jumps to current address infinitelly. a dirty way to pausing 
-;		execution
-;
-; D.03: this two byte value represents end of a boot sector. must be defined. otherwise
-;		BIOS will reject our code
+mov bx, second_sector_message
+call print
 
+times 512 db 0x0
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;DOCUMENTATION
+;;
+;;D.01: 0x7c00 is a special address in x86 architecture. it represents a boot sector
+;;      starting point. our code will be placed into memory after this address when you
+;;      press power button of the PC
+;;		
+;;D.02: this is special for nasm (netwide assembler). '$' symbol represents the "current
+;; 		address". it jumps to current address infinitelly. a dirty way to pausing 
+;;		execution. you can also do:
+;;
+;;      loop:
+;;      	jmp loop
+;;
+;;D.03: this two byte value represents "end of a boot sector". must be defined. otherwise
+;;		BIOS will reject our code
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- asm/utils.asm
+++ asm/utils.asm
@@ -90,7 +90,7 @@ print_nl:
 ;;	@description:	gets one line input like username or password. finishes when ENTER 
 ;;					key is pressed
 ;;
-;;	@parameters:	
+;;	@parameters:    
 ;;
 ;;  @example_usage: 
 ;;					call get_one_line_input
@@ -123,3 +123,74 @@ get_one_line_input:
 
 		popa							;; restore all registers
 		ret								;; return 0;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;	@function:		load_into_memory
+;;
+;;	@description:	this function reads data from HARD DISK or FLASH DRIVE and puts this
+;;                  data into RAM. useful function for loading Kernel into RAM
+;;
+;;	@parameters:    cl ;; from which sector we want to start reading
+;;					al ;; how many sectors we want to read		
+;;
+;;					(see D.01 for *sector* meaning)
+;;                
+;;
+;;  @example_usage: 
+;;                  mov cl, 0x02
+;;                  mov al, 0x01
+;;					call load_into_memory
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+load_into_memory:
+	pusha								;; save all registers into stack
+		mov ah, 0x02					;; preparation (mendatory)
+
+			;; code to read from hard disk
+			mov dh, 0x0                 ;; head number (see D.03)
+			mov ch, 0x0                 ;; cylinder number (see D.03)
+			mov dl, 0x80				;; read from Hard Disk (see D.02)
+
+			;; code to write to RAM
+			mov bx, 0					;; prepare a value for extra segment (es)
+			mov es, bx					;; load extra segment (es) with zero
+			mov bx, 0x7c00 + 0x200      ;; write data from hdd into address 0x7e00 (see D.04)
+		int 0x13						;; execute (mendatory)
+	popa								;; restore all registers from stack
+	ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;DOCUMENTATION:
+;;
+;;D.01: sector is just a 512 byte data. this concept is mostly used in hard disk
+;;      like devices. hard disks or flash drives are not like RAM. we cannot read them
+;;      byte by byte. so we specify a *sector* instead of *byte*.
+;;
+;;      sector numbers are also start from 1. which seems awkward from a programmer's 
+;;      perspective. our boot sector is always on sector 1.
+;;
+;;D.02: when reading from hard disk, dl register must be set 0x80. here is a table:
+;;
+;;      hard drive:  0x80
+;;      ssd        : 0x80
+
+;;      floppy disk: 0x00
+;;      flash drive: 0x00
+;;
+;;      fun fact: flash drives actually emulate floppy disks. as you know floppy disks
+;;      are obsolete and flash drives are huge floppy disks. this is why CPU thinks a
+;;      flash drive is actually a floppy.. we can say the same thing for hdd and ssd where
+;;      ssd emulates hdd :)
+;;
+;;D.03: here is a great article that teaches a lot of hard drive internals and how it
+;;      actually works: https://en.wikipedia.org/wiki/Cylinder-head-sector
+;;
+;;D.04: 0x200 is actually 512 in decimal. so our boot sector was starting on 0x7c00 right?
+;;      now we send bx to the bottom of our boot sector and say "hey bx! start loading
+;;      that 512 byte long sector which I read from hard disk into memory where you stand!"
+;;
+;;      we could also put 0x7e00 into bx which is the result of 0x7c00 + 0x200
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
